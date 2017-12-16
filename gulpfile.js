168d4e87bf267fe5eef3209d8f267786eaf26e09
Mka <mka281@icloud.com>
2017-12-16 04:19:20 +0300

    add CSS architecture

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require("gulp-watch"),
 postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
-nested = require("postcss-nested");
+nested = require("postcss-nested"),
+cssImport = require("postcss-import");
+
 
 gulp.task("default", function() {
   console.log("a new task")
@@ -15,7 +17,7 @@ gulp.task("html", function() {
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles"));
 });
 
