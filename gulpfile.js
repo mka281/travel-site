fdd3d6af0c6d5f4b598fe349896f8089141b1542
Mka <mka281@icloud.com>
2017-12-16 12:40:22 +0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
-cssImport = require("postcss-import");
+cssImport = require("postcss-import")
+browserSync = require("browser-sync").create();
 
 
 gulp.task("default", function() {
@@ -22,11 +23,21 @@ gulp.task("styles", function(){
 });
 
 gulp.task("watch", function() {
-  gulp.watch("./app/index.html", function () {
-    gulp.start("html");
-  })
-
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+  watch("./app/index.html", function () {
+    browserSync.reload();
+  });
   watch("./app/assets/styles/**/*.css", function(){
-    gulp.start("styles");
+    gulp.start("cssInject");
   });
 });
+
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/styles.css")
+    .pipe(browserSync.stream())
+})
